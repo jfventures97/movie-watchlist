89b72fd8a263c75d2e4a7d45b99bc4d73534ad56
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-09-27 14:15:29 +0800

    added api

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+const searchButton = document.getElementById('search-button')
+const movieContainer= document.getElementById('movie-container')
+
+searchButton.addEventListener('click', function(){
+
+    fetch("http://www.omdbapi.com/?apikey=8092a520&s=blade+runner")
+    .then(res=> res.json())
+    .then(data => console.log(data))
+
+    movieContainer.innerHTML = ``
+
+})
