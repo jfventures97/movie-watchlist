18ebd7dfd3bacbc664ed0f6ff4160b665a075b80
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-09-28 07:58:53 +0800

    halfway done with homepage

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 
 
 const searchButton = document.getElementById('search-button')
-const movieContainer= document.getElementById('movie-container')
+const movieContainer= document.getElementById('inner-movie-container')
 
 searchButton.addEventListener('click', function(){
 
     fetch("http://www.omdbapi.com/?apikey=8092a520&s=blade+runner")
     .then(res=> res.json())
     .then(data => { 
-        
-        console.log(data)
+        movieContainer.innerHTML ="";
+        data.Search.forEach(movie=> {
+            fetch(`http://www.omdbapi.com/?apikey=8092a520&i=${movie.imdbID}&plot=short`)
+            .then(res=>res.json())
+            .then(details => {
+                console.log(details)
+                movieContainer.innerHTML+= `
+                <div class="movie-card">
+                                <div class="left-side">
+                                    <img class="poster-image" src="${details.Poster}" alt="${details.Title}"> 
+                                </div>
 
-    movieContainer.innerHTML = `
-    <div id='movie-poster'>
-        <img src=${data.Search[0].Poster}> 
-    </div>
-    <div id='movie-info-container'>
-        <div id='row-one'>
-        <h1>${data.Search[0].Title}</h1>
-        <p>${data.Search[0].review} </p>
-        </div>
-    </div>
-    `
+                                <div id="right-side">
+                                    <div class="first-row">
+                                        <h2 class="font18">${details.Title}</h2>
+                                        <p class="font12"> ${details.imdbRating}</p>
+                                    </div>
+                                    <div class="second-row">
+                                        <p class="font12"> ${details.Runtime} </p>
+                                        <p class="font12"> ${details.Genre} </p>
+                                    </div>
+                                    <p class="font14">${details.Plot}</p>
+                                </div>
+                            </div>
+                `
+
+            })
+        } )
+
+    
     }
 ) } )
