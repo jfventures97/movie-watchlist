12132b345e9e29ae9846884ca47d50e93bf3894b
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-09-27 17:35:36 +0800

    got the poster to work

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ searchButton.addEventListener('click', function(){
 
     fetch("http://www.omdbapi.com/?apikey=8092a520&s=blade+runner")
     .then(res=> res.json())
-    .then(data => console.log(data))
+    .then(data => { 
+        
+        console.log(data)
 
-    movieContainer.innerHTML = ``
-
-})
+    movieContainer.innerHTML = `
+    <div id='movie-poster'>
+        <img src=${data.Search[0].Poster}> 
+    </div>
+    `
+    }
+) } )
